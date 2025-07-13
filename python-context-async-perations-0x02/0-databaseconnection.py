ea2ca025492b3513afc08deac4298e5db9f8c053
nommadj <mainamasha254@gmail.com>
2025-07-13 10:05:33 +0300

    py context

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -1,14 +1,18 @@
 import sqlite3
 
 class DatabaseConnection:
+    def __init__(self, db_name='users.db'):
+        self.db_name = db_name
+
     def __enter__(self):
-        self.conn = sqlite3.connect('users.db')
+        self.conn = sqlite3.connect(self.db_name)
         self.cursor = self.conn.cursor()
         return self.cursor
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.conn.close()
 
+# Use the context manager
 with DatabaseConnection() as cursor:
     cursor.execute("SELECT * FROM users")
     results = cursor.fetchall()
